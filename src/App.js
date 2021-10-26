0d9834895a2ce0cf6f142365fc77aaaa9f5b956f
Filipe Melo <filipexpert63@gmail.com>
2021-10-26 15:44:17 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Fruits from "./components/fruits/Fruits"
+import "./App.css";
+import Fruits from "./components/fruits/Fruits";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Fruits />
+        <Fruits />
       </header>
     </div>
   );
--- src/components/fruits/Fruits.jsx
+++ src/components/fruits/Fruits.jsx
@@ -1,33 +1,29 @@
-import {useState} from "react";
-import "./style.css"
+import { useState } from "react";
+import "./style.css";
 
-const arrFruits = [
+const Fruits = () => {
+  const [fruits, setFruits] = useState([
     { name: "banana", color: "yellow", price: 2 },
     { name: "cherry", color: "red", price: 3 },
     { name: "strawberry", color: "red", price: 4 },
-]
+  ]);
 
-const Fruits = () => {
-    
-    const [fruits, setFruits] = useState(arrFruits)
-    
-    const redFruits = () =>{
-        return setFruits(arrFruits.filter((item) => item.color === "red"))
-    }
-    return(
-        <div>
-            
-            <ul>
-                {fruits.map((item) => <li>{item.name}</li>)}
-            </ul>
+  const redFruits = () => {
+    return setFruits(fruits.filter((item) => item.color === "red"));
+  };
+  return (
+    <div>
+      <ul>
+        {fruits.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
 
-            <button onClick={redFruits} className="buttonFruits">
-                Mostrar frutas vermelhas
-            </button>
+      <button onClick={redFruits} className="buttonFruits">
+        Mostrar frutas vermelhas
+      </button>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-
-export default Fruits
+export default Fruits;
